dfc12894ce4e2ad426d7f579da2a21ff795f49bf
Guido Accardo <gaccardo@gmail.com>
2013-11-30 00:32:04 -0300

    Template raw added. Log generator has new random data

--- resources/log_generator.py
+++ resources/log_generator.py
@@ -3,9 +3,13 @@
 import time
 import random
 
-IPS = ['10.0.0.1', '10.0.0.2', '10.0.0.3', '10.0.0.4', '10.0.0.5']
+IPS = ['10.0.0.1', '10.0.0.2', '10.0.0.3', '10.0.0.4', '10.0.0.5','10.0.0.6']
 CODES = ['TCP_MISS/200', 'TCP_MISS/204', 'TCP_HIT/200']
 METHODS = ['GET', 'POST']
+DOMAINS = ['http://www.google.com',
+           'http://www.clarin.com.ar',
+           'http://www.guidoaccardo.com.ar']
+DOCUMENTS = ['text/html', 'text/plain']           
 
 class LogGenerator( object ):
 
@@ -14,7 +18,7 @@ class LogGenerator( object ):
 
 	def run(self):
 		f_p = open(self.file, 'w')
-		LINES = 335000
+		LINES = 1500
 
 		while LINES > 0:
 			f_p.write("%.10f\t %s %s %s %s %s %s %s %s %s\n" % ( time.time(),
@@ -23,7 +27,7 @@ class LogGenerator( object ):
 			                                                random.choice(CODES),
 			                                                random.randint(50, 2000),
 			                                                random.choice(METHODS),
-			                                                'http://www.google.com',
+			                                                random.choice(DOMAINS),
 			                                                '-',
 			                                                'DIRECT/125.23.216.203',
 			                                                'text/html'))
