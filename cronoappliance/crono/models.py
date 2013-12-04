8cd777b3f5697e459f5a18230ca7d587357bd845
Guido Accardo <gaccardo@gmail.com>
2013-12-04 10:49:57 -0300

    Access ingresor using mysqlalchemy. The desition of which data model nor engines hasn't been taken yet

--- cronoappliance/crono/models.py
+++ cronoappliance/crono/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Access(models.Model):
+	time = models.FloatField(max_length=40)
+	elapsed = models.IntegerField(default=0)
+	ip = models.CharField(max_length=16)
+	code = models.CharField(max_length=25)
+	data = models.IntegerField(default=0)
+	method = models.CharField(max_length=8)
+	url = models.CharField(max_length=100)
+	date = models.DateTimeField(auto_now=False)
+
+class LastKey(models.Model):
+	time = models.FloatField(max_length=40)
--- cronoappliance/cronoappliance/settings.py
+++ cronoappliance/cronoappliance/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'cronoappliance.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crono',
+        'USER': 'crono',
+        'PASSWORD': 'ppp123',
+        'HOST': 'localhost',
+        'PORT': '3389',
     }
 }
 
