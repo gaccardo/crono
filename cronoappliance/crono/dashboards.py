88ff4abceb262b39757ee391edcac9b4ec1c0014
Guido Accardo <gaccardo@gmail.com>
2013-12-04 15:04:40 -0300

    Top ten filter by date is working

--- cronoappliance/crono/dashboards.py
+++ cronoappliance/crono/dashboards.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from django.template import RequestContext, loader
 from django.db.models import Count
 from django.db.models.query import QuerySet
+from django.http import Http404, HttpResponse
+from django.utils import simplejson
 
 from crono.models import Access
+
+import time
 
 def get_ips_info(request):
 	"""
@@ -69,11 +73,20 @@ def get_ips_info(request):
 	pass
 
 def get_top_ten(request):
-	accesses = Access.objects.values('url').annotate(count=Count('url')).order_by('-count')[0:10]
-
-	context = RequestContext(request, {
-		'accesses': accesses
-	})
+	context = RequestContext(request, {})
 	return render(request, 'crono/dashboards/top_ten.html', context)
 
 	#timestamp = time.mktime( time.strptime(datestring, '%d/%m/%Y') )
+
+def get_top_ten_range(request, date_from, date_to):
+	timestamp_from = time.mktime( time.strptime(date_from, '%m_%d_%Y') )
+	timestamp_to = time.mktime( time.strptime(date_to, '%m_%d_%Y') )
+	
+	accesses = Access.objects.filter(time__gte=timestamp_from,
+		                               time__lt=timestamp_to).values('url').annotate(count=Count('url')).order_by('-count')[0:10]
+	result_accesses = list()
+
+	for acc in accesses:
+		result_accesses.append({'url': acc['url'], 'count': acc['count']})
+
+	return HttpResponse(simplejson.dumps({'urls': result_accesses}))
--- cronoappliance/cronoappliance/urls.py
+++ cronoappliance/cronoappliance/urls.py
@@ -21,6 +21,8 @@ urlpatterns = patterns('',
     url(r'^menu', menu.represent, name='menu'),
     url(r'^backend/layout', 'crono.views.layout'),
 
+    url(r'^backend/top10byrange/(?P<date_from>\w+)/(?P<date_to>\w+)', 'crono.dashboards.get_top_ten_range', name='top10byrange'),
+
 	#url(r'^couch/last_key', 'crono.couch.last_key', name='last_key'),
 	#url(r'^couch/denied_requests', 'crono.couch.denied_requests'),
 	#url(r'^couch/misses_requests/$', 'crono.couch.misses_requests'),
